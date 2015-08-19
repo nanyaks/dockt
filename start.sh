cc7004636bc778a6bfb2c3e2d9800878b7deb5a6
Loknan Nanyak <nanyaks@gmail.com>
2015-08-19 12:25:43 +0100

    Added code to check if docker is running.

--- start.sh
+++ start.sh
@@ -1,3 +1,27 @@
-# Start and configure the docker environment.
-
 #!/bin/bash
+
+##################################################################
+#
+#   DS Docker scripts
+#
+#   @author Loknan S. Nanyak - @nanyaks
+#   @date   Wed Aug 19 12:15:47 2015
+##################################################################
+
+# Error handling
+
+# Our naive error handling routine
+# This is used to send error signals to the user to stdout
+function error_exit {
+    echo "$1" 1>&2
+    exit 1
+}
+
+RUNNING=`pgrep docker`
+if [ "$?" = "0" ]; then
+    error_exit "Docker already running. Aborting!"
+fi
+
+# TRIAL=('LOKNAN', 'NANYAK')
+
+# echo ${#RUNNING[@]}
